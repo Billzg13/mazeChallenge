fca746c2586dc6f76605ee5ed8e792609a1337cc
Billzg13 <Billzg1234@gmail.com>
2020-09-27 21:32:26 +0300

    add new git ignore

---
+++ Readme.md
@@ -0,0 +1,12 @@
+# Maze challenge
+
+
+
+## Build project: 
+ * mvn package
+
+## RUN:  
+* java -cp ./target/maze-1.0-SNAPSHOT.jar Main
+
+
+
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -92,3 +92,17 @@ public class Main {
         return userInput;
     }
 }
+
+
+/*
+
+FROM maven:3.5.4-jdk-8-slim
+WORKDIR /usr/src/java-code
+COPY . /usr/src/java-code/
+RUN mvn package
+
+WORKDIR /usr/src/java-app
+RUN cp /usr/src/java-code/target/*.jar ./app.jar
+EXPOSE 8000
+CMD ["java", "-jar", "app.jar"]
+ */
